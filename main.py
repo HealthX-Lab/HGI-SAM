2e303effbb9506037baf5292d47a9316ca3f9b44
Amirhossein <ah.rasoulian@gmail.com>
2023-01-22 16:34:12 -0500

    added confusion matrix and metrics calculators, early stopping, and binarization methods.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import cv2
 from tqdm import tqdm
 from dataset import *
-from models.swin_transformer_v2 import *
 from torchvision import transforms
 from utils import *
 import torch.nn as nn
@@ -11,20 +10,23 @@ from train import *
 import torch
 from preprocessing import *
 from torch.utils.data import DataLoader
+import timm
 
 
 def main():
-    src = r'D:\Datasets\rsna-ich-nifty'
+    print(timm.list_models('*swin*'))
+    src = r'C:\rsna-ich-nifty'
     train_files, validation_files = rsna_3d_train_validation_split(src)
+    img_size = 224
     transform = transforms.Compose([
-        transforms.Resize(int(224 * 1.2)),
-        transforms.CenterCrop(224),
+        transforms.Resize(int(img_size * 1.2)),
+        transforms.CenterCrop(img_size),
     ])
-    train_ds = RSNAICHDataset3D(src, train_files[756:1000], windows=[(40, 80), (40, 80), (40, 80)], transform=transform)
-    train_dl = DataLoader(train_ds, batch_size=4, collate_fn=rsna_collate_fn)
-    validation_ds = RSNAICHDataset3D(src, validation_files[:100], windows=[(40, 80), (40, 80), (40, 80)], transform=transform)
-    validation_dl = DataLoader(validation_ds, batch_size=4, collate_fn=rsna_collate_fn)
-    model = SwinTransformerV2(num_classes=1)
+    train_ds = RSNAICHDataset3D(src, train_files[1000:1100], windows=[(40, 80)], transform=transform)
+    train_dl = DataLoader(train_ds, batch_size=1, collate_fn=rsna_collate_fn, shuffle=True)
+    validation_ds = RSNAICHDataset3D(src, validation_files[500:510], windows=[(40, 80)], transform=transform)
+    validation_dl = DataLoader(validation_ds, batch_size=1, collate_fn=rsna_collate_fn)
+    model = timm.create_model('swin_base_patch4_window7_224', in_chans=1, num_classes=1)
     opt = torch.optim.Adam(model.parameters())
     for i in range(10):
         m = train_one_epoch(model, opt, FocalBCELoss(), train_dl, validation_dl)
--- utils.py
+++ utils.py
@@ -2,96 +2,10 @@ import torch.nn as nn
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
-from rwightman_files.swin_transformer import window_reverse
 import cv2
 import torch.nn.functional as F
 from sklearn import metrics
 from skimage.filters import threshold_otsu
-
-
-class WeightedBCELoss:
-    def __init__(self, pos_neg_weights, subtypes_weights, reduction='sum'):
-        self.pos_neg_weights = pos_neg_weights.T
-        self.subtypes_weights = subtypes_weights
-        self.reduction = reduction
-
-    def __call__(self, pred, gt):
-        sigmoid = torch.sigmoid(pred)
-        bce = -(self.pos_neg_weights[1] * gt * torch.log(sigmoid) +
-                self.pos_neg_weights[0] * (1 - gt) * torch.log(1 - sigmoid))
-
-        if self.reduction == 'sum':
-            reduced_bce = bce.sum(dim=0)
-        else:
-            reduced_bce = bce.mean(dim=0)
-        return (reduced_bce * self.subtypes_weights).mean()
-
-
-class FocalBCELoss:
-    def __init__(self, subtypes_weights, gamma=5, reduction='sum'):
-        self.subtypes_weights = subtypes_weights
-        self.gamma = gamma
-        self.reduction = reduction
-
-    def __call__(self, pred, gt):
-        sigmoid = torch.sigmoid(pred)
-        bce = -(gt * ((1 - sigmoid) ** self.gamma) * torch.log(sigmoid) +
-                (1 - gt) * (sigmoid ** self.gamma) * torch.log(1 - sigmoid))
-
-        if self.reduction == 'sum':
-            reduced_bce = bce.sum(dim=0)
-        else:
-            reduced_bce = bce.mean(dim=0)
-
-        return (reduced_bce * self.subtypes_weights).mean()
-
-
-class WeightedFocalBCELoss:
-    def __init__(self, pos_neg_weights, subtypes_weights, gamma=4, reduction='sum'):
-        self.pos_neg_weights = pos_neg_weights.T
-        self.subtypes_weights = subtypes_weights
-        self.gamma = gamma
-        self.reduction = reduction
-
-    def __call__(self, pred, gt):
-        sigmoid = torch.sigmoid(pred)
-        bce = -(self.pos_neg_weights[1] * gt * ((1 - sigmoid) ** self.gamma) * torch.log(sigmoid) +
-                self.pos_neg_weights[0] * (1 - gt) * (sigmoid ** self.gamma) * torch.log(1 - sigmoid))
-
-        if self.reduction == 'sum':
-            reduced_bce = bce.sum(dim=0)
-        else:
-            reduced_bce = bce.mean(dim=0)
-
-        return (reduced_bce * self.subtypes_weights).mean()
-
-
-class EarlyStopping:
-    def __init__(self, model: nn.Module, patience: int, path_to_save: str, gamma=0):
-        assert patience > 0, 'patience must be positive'
-        self.model = model
-        self.patience = patience
-        self.gamma = gamma
-        self.path_to_save = path_to_save
-
-        self.min_loss = np.Inf
-        self.early_stop = False
-        self.counter = 0
-
-    def __call__(self, val_loss):
-        if val_loss + self.gamma < self.min_loss:
-            print("val loss decreased from {} to {}".format(self.min_loss, val_loss))
-            self.min_loss = val_loss
-            self.counter = 0
-            save_model(self.model, self.path_to_save)
-        else:
-            save_model(self.model, self.path_to_save + r'early.pt')
-            self.counter += 1
-            if self.counter == self.patience:
-                print('early stop')
-                self.early_stop = True
-            else:
-                print('early stopping counter: {} of {}'.format(self.counter, self.patience))
 
 
 class ConfusionMatrix:
@@ -99,15 +13,26 @@ class ConfusionMatrix:
         self.device = device
         self.predictions = []
         self.ground_truth = []
+
+        self.losses = []
         self.dices = []
+        self.number_of_samples = 0
+
         self.true_positives = None
         self.true_negatives = None
         self.false_positives = None
         self.false_negatives = None
 
-    def __call__(self, pred, gt):
-        self.predictions.append(pred)
-        self.ground_truth.append(gt)
+    def add_prediction(self, pred, gt):
+        self.predictions.extend(list(pred))
+        self.ground_truth.extend(list(gt))
+        self.number_of_samples += len(gt)
+
+    def add_loss(self, loss):
+        self.losses.append(loss)
+
+    def add_dice(self, dice):
+        self.dices.append(dice)
 
     def compute_confusion_matrix(self):
         self.predictions = torch.stack(self.predictions).to(self.device)
@@ -118,6 +43,8 @@ class ConfusionMatrix:
         self.true_negatives = torch.sum((1 - pred_out) * (1 - self.ground_truth), dim=0)
         self.false_positives = torch.sum(pred_out * (1 - self.ground_truth), dim=0)
         self.false_negatives = torch.sum((1 - pred_out) * self.ground_truth, dim=0)
+
+        self.losses = sum(self.losses) / self.number_of_samples
 
     def get_accuracy(self):
         numerator = self.true_positives + self.true_negatives
@@ -159,6 +86,52 @@ class ConfusionMatrix:
         return np.array(scores)
 
 
+class FocalBCELoss:
+    def __init__(self, gamma=2, reduction='sum'):
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def __call__(self, pred, gt):
+        sigmoid = torch.sigmoid(pred)
+        bce = -(gt * ((1 - sigmoid) ** self.gamma) * torch.log(sigmoid) +
+                (1 - gt) * (sigmoid ** self.gamma) * torch.log(1 - sigmoid))
+
+        if self.reduction == 'sum':
+            reduced_bce = bce.sum(dim=0)
+        else:
+            reduced_bce = bce.mean(dim=0)
+
+        return reduced_bce
+
+
+class EarlyStopping:
+    def __init__(self, model: nn.Module, patience: int, path_to_save: str, gamma=0):
+        assert patience > 0, 'patience must be positive'
+        self.model = model
+        self.patience = patience
+        self.gamma = gamma
+        self.path_to_save = path_to_save
+
+        self.min_loss = np.Inf
+        self.early_stop = False
+        self.counter = 0
+
+    def __call__(self, val_loss):
+        if val_loss + self.gamma < self.min_loss:
+            print("val loss decreased from {} to {}".format(self.min_loss, val_loss))
+            self.min_loss = val_loss
+            self.counter = 0
+            save_model(self.model, self.path_to_save)
+        else:
+            save_model(self.model, self.path_to_save + r'early.pt')
+            self.counter += 1
+            if self.counter == self.patience:
+                print('early stop')
+                self.early_stop = True
+            else:
+                print('early stopping counter: {} of {}'.format(self.counter, self.patience))
+
+
 def save_model(model: nn.Module, path: str):
     torch.save(model.state_dict(), path)
 
@@ -167,100 +140,11 @@ def load_model(model: nn.Module, path: str):
     model.load_state_dict(torch.load(path))
 
 
-def get_per_class_accuracy(pred, gt):
-    predictions = torch.round(torch.sigmoid(pred))
-    return (predictions == gt).sum(dim=0) / len(gt)
-
-
 def plot_diagram(*metrics):
     fig, axes = plt.subplots(len(metrics))
     for i in range(len(metrics)):
         axes[i].plot(range(1, 1 + len(metrics[i])), metrics[i])
     plt.show()
-
-
-def get_attentions(att_dict, layer_block):
-    def hook(model, inp, out):
-        att_dict[layer_block] = out.detach()
-
-    return hook
-
-
-def get_attention_mask(attentions, g_cam=None, img_size=384, patch_size=4, window_size=12, device='cpu'):
-    func = _get_layer_attention_mask
-    softmax_layer = nn.Softmax(dim=1)
-
-    mask0 = func(attentions, img_size, patch_size, window_size, 0, list(range(2)), device)
-    mask1 = func(attentions, img_size, patch_size, window_size, 1, list(range(2)), device)
-    mask2 = func(attentions, img_size, patch_size, window_size, 2, list(range(18)), device)
-    mask3 = func(attentions, img_size, patch_size, window_size, 3, list(range(2)), device)
-    # cv2.imshow('last', mask3.cpu().numpy())
-    # cv2.imshow('second last', mask2.cpu().numpy())
-
-    if g_cam is None:
-        f_mask = mask3 * mask2 * mask1 * mask0
-    else:
-        g_cam = torch.tensor(g_cam, device=device)
-        f_mask = g_cam * mask2 * mask1 * mask0
-
-    H, W = f_mask.shape
-    softmax = f_mask
-    softmax = softmax.reshape(1, -1)
-    softmax = softmax_layer(softmax)
-    softmax = softmax.reshape(H, W)
-    softmax = (softmax - softmax.min()) / (softmax.max() - softmax.min())
-    # cv2.imwrite(r'C:\rsna-ich\sample images\softmax.bmp', 255 * softmax.cpu().numpy())
-
-    return softmax
-
-
-def _get_layer_attention_mask(attentions: dict, img_size: int, patch_size: int, window_size: int,
-                              layer: int, blocks: list, device='cpu'):
-    total_tokens = img_size // patch_size
-    layer_tokens = total_tokens // (2 ** layer)
-    shift_size = window_size // 2
-
-    avg_pooling = nn.AdaptiveAvgPool2d((1, window_size * window_size))
-    masks = []
-    for block_index in blocks:
-        att = attentions[f'{layer}_{block_index}']
-        att = torch.mean(att, dim=1)  # mean over heads
-        mask = avg_pooling(att).reshape(-1, window_size * window_size)  # pool over target sequences
-
-        mask = mask.reshape(-1, window_size, window_size, 1)
-        mask = window_reverse(mask, window_size, layer_tokens, layer_tokens)
-        # if layer == 0:
-        #     print(mask.shape)
-        #     x = mask.squeeze().cpu().numpy()
-        #     x = (x - x.min()) / (x.max() - x.min())
-        #     cv2.imwrite(rf'C:\rsna-ich\sample images\maps\w{block_index}.bmp', x * 255)
-        if block_index % 2 == 1:  # reverse shift for odd blocks
-            mask = torch.roll(mask, shifts=(shift_size, shift_size), dims=(1, 2))
-            # if layer == 0:
-            #     x = mask.squeeze().cpu().numpy()
-            #     x = (x - x.min()) / (x.max() - x.min())
-            #     cv2.imwrite(rf'C:\rsna-ich\sample images\maps\sw{block_index}.bmp', x * 255)
-        mask = mask.permute(0, 3, 1, 2)
-        masks.append(mask)
-
-    final_masks = []
-    for i in range(0, len(masks), 2):
-        swin_block_mask = masks[i] * masks[i + 1]
-        # if layer == 0:
-        #     x = swin_block_mask.squeeze().cpu().numpy()
-        #     x = (x - x.min()) / (x.max() - x.min())
-        #     cv2.imwrite(r'C:\rsna-ich\sample images\maps\mult.bmp', x * 255)
-        final_masks.append(swin_block_mask)
-
-    all_masks = torch.stack(final_masks)
-    final_mask = torch.mean(all_masks, dim=0)
-
-    final_mask = (final_mask - final_mask.min()) / (final_mask.max() - final_mask.min())
-
-    # cv2.imwrite(rf'C:\rsna-ich\sample images\layer {layer} attention.bmp', 255 * final_mask.squeeze().cpu().numpy())
-    final_mask = F.interpolate(final_mask, size=(img_size, img_size), mode='bilinear').squeeze()
-    # cv2.imwrite(rf'C:\rsna-ich\sample images\layer {layer} interpolated.bmp', 255 * final_mask.cpu().numpy())
-    return final_mask
 
 
 def dice_metric(predicted_mask, gt_mask):
